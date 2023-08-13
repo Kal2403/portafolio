5fa9d871ab6a91124f0568792b02eb1afcebe995
Kal2403 <kal2403199255@gmail.com>
2023-08-13 21:37:03 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: './assets/prueba1.jpg'
+  },
+  {
+    id: 2,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: "./assets/prueba1.jpg"
+  },
+  {
+    id: 3,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: "./assets/prueba1.jpg"
+  },
+  {
+    id: 4,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: "./assets/prueba1.jpg"
+  },
+  {
+    id: 5,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: "./assets/prueba1.jpg"
+  },
+  {
+    id: 6,
+    name: "Multi-Post Stories Gain+Glory",
+    tecnologys: ["React", "JavaScript", "HTML", "CSS"],
+    url: "",
+    img: ""
+  }
+]
+
+export const cardTechData = [
+  {
+    imagen: "../assets/icon-languages.svg",
+    name: "Lenguajes",
+    listTech: ["JavaScript", "Ruby", "HTML", "CSS"],
+  },
+  {
+    imagen: "",
+    name: "Lenguajes",
+    listTech: ["Reacy", "Ruby", "RSpec", "Copy"],
+  },
+  {
+    imagen: "",
+    name: "Lenguajes",
+    listTech: ["DataBase", "CLI", "Version Control", "Web Development"],
+  },
+]
