05ca7317222f946130c550a0c2f4f5aa4db50b35
Kal2403 <kal2403199255@gmail.com>
2023-08-13 21:35:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './containers/Home/Home';
+import { About } from './containers/About/About';
+import { projects, cardTechData } from './data.js'
+import Portfolio from './containers/Portfolio/Portfolio';
 
 const App = () => {
+
   return (
     <div className="App">
-      <h1>Mi Portafolio</h1>
+      <Home />
+      <Portfolio projects={projects}/>
+      <About cardTechData={cardTechData} />
     </div>
   );
 }
