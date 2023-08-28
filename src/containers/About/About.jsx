1250fba80e3474e1714ac907392d3a18af3cac27
Kal2403 <kal2403199255@gmail.com>
2023-08-28 22:08:12 +0200

    Update components

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -5,10 +5,14 @@ import './About.css';
 export const About = (props) => {
   return (
     <div className='about-container'>
-      <h1>About me</h1>
-      <p className='about-container_text'> I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded,
+      <div className='about-container_description'>
+        <div className='about-container_second'>
+          <h1>About me</h1>
+          <p className='about-container_text'> I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded,
 don't hesitate to contact me.</p>
-      <button className='about-btn'>Get my resume</button>
+          <button className='about-btn'>Get my resume</button>
+        </div>
+      </div>
       <div className='about-container_cardsTech'>
         {
           props.cardTechData.map((elem) => (
@@ -20,7 +24,6 @@ don't hesitate to contact me.</p>
           ))
         }
       </div>
-      
     </div>
   )
 }
