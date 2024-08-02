4aca3c4cb6514ea625aefd3afdb78a418ac0323a
Kal2403 <kal2403199255@gmail.com>
2024-08-02 15:58:57 +0200

    Added icons and starting with responsive divices

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import './Nav.css'
-import { Link } from 'react-scroll'
+import React, { useState } from 'react';
+import './Nav.css';
+import { Link } from 'react-scroll';
+import { MdOutlineMenu } from "react-icons/md";
 
 const Nav = () => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const closeMenu = () => {
+    setOpenMenu(false);
+  };
+
   return (
     <div className='nav-container'>
-      <ul className='nav-container_ul'>
-        <li><Link to='home' smooth={true} duration={500}>Home</Link></li>
-        <li><Link to='portfolio' smooth={true} duration={500}>Portfolio</Link></li>
-        <li><Link to='about' smooth={true} duration={500}>About</Link></li>
-        <li><Link to='contact' smooth={true} duration={500}>Contact</Link></li>
-      </ul>
+      <h2>Kal<span>Tech</span></h2>
+      {
+        openMenu && (
+          <ul className={`nav-container_ul ${openMenu ? 'open' : ''}`}>
+            <li><Link to='home' smooth={true} duration={700} onClick={closeMenu}>Home</Link></li>
+            <li><Link to='portfolio' smooth={true} duration={700} onClick={closeMenu}>Portfolio</Link></li>
+            <li><Link to='about' smooth={true} duration={700} onClick={closeMenu}>About</Link></li>
+            <li><Link to='contact' smooth={true} duration={700} onClick={closeMenu}>Contact</Link></li>
+          </ul>
+      )}
+      <div onClick={toggleMenu} className="menu-container">
+        <MdOutlineMenu className='menu'/>
+      </div>
     </div>
   )
 }
