7a2b49330618ed1ba4b11dcf2ed8e5e1c46f2655
Cristhian Castillo <kal2403199255@gmail.com>
2024-08-06 06:15:25 +0200

    Update nav and adding media querys

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -18,21 +18,12 @@ const Nav = () => {
   return (
     <div className='nav-container'>
       <h2>Kal<span>Tech</span></h2>
-      <ul className='nav-container_ul-desk'>
-        <li><Link to='home' smooth={true} duration={700}>Home</Link></li>
-        <li><Link to='portfolio' smooth={true} duration={700}>Portfolio</Link></li>
-        <li><Link to='about' smooth={true} duration={700}>About</Link></li>
-        <li><Link to='contact' smooth={true} duration={700}>Contact</Link></li>
+      <ul className={`nav-container_ul ${openMenu ? 'open' : ''}`}>
+        <li><Link to='home' smooth={true} duration={700} onClick={closeMenu}>Home</Link></li>
+        <li><Link to='portfolio' smooth={true} duration={700} onClick={closeMenu}>Portfolio</Link></li>
+        <li><Link to='about' smooth={true} duration={700} onClick={closeMenu}>About</Link></li>
+        <li><Link to='contact' smooth={true} duration={700} onClick={closeMenu}>Contact</Link></li>
       </ul>
-      {
-        openMenu && (
-          <ul className={`nav-container_ul ${openMenu ? 'open' : ''}`}>
-            <li><Link to='home' smooth={true} duration={700} onClick={closeMenu}>Home</Link></li>
-            <li><Link to='portfolio' smooth={true} duration={700} onClick={closeMenu}>Portfolio</Link></li>
-            <li><Link to='about' smooth={true} duration={700} onClick={closeMenu}>About</Link></li>
-            <li><Link to='contact' smooth={true} duration={700} onClick={closeMenu}>Contact</Link></li>
-          </ul>
-      )}
       <div onClick={toggleMenu} className="me55nu-container">
         <MdOutlineMenu className='menu'/>
       </div>
