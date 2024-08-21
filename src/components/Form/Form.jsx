852c6bcde350020855680f86cd908571a0838963
Cristhian Castillo <kal2403199255@gmail.com>
2024-08-21 06:02:54 +0200

    Integrated EmailJS into contact form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
-import './Form.css'
+import './Form.css';
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      //'service',
+      //'template',
+      e.target,
+      //'user-id'
+    ).then((result) => {
+      console.log(result.text);
+      alert('Mensaje enviado con exito!');
+    }, (error) => {
+      console.log(error.text);
+      alert('Hubo un error al enviar el mensaje, inténtalo de nuevo.');
+    });
+    
+    e.target.reset(); // To clean Form
+  }
+
   return (
-    <div className='form-container'>
+    <form onSubmit={sendEmail} className='form-container'>
       <div className='form-container_name'>
-        <input type="text" placeholder='First Name'/>
-        <input type="text" placeholder='Last Name'/>
+        <label htmlFor="name">Name</label>
+        <input id='name' name='name' type="text" placeholder='Your Name'/>
       </div>
       <div className='form-container_emailComments'>
-        <input type="email" placeholder='Email Addres'/>
-        <textarea name="textarea" cols="30" rows="10" placeholder='Hey ld like to connect to...'></textarea>
+        <label htmlFor="email">Email</label>
+        <input id='email' name='email' type="email" placeholder='Your Email'/>
+        <label htmlFor="message">Message</label>
+        <textarea id='message' name="message" cols="30" rows="10" placeholder='Hey ld like to connect to...'></textarea>
       </div>
-      <button className='form-btn'>Get in touch</button>
-    </div>
+      <button type='submit' className='form-btn'>Get in touch</button>
+    </form>
   )
 }
 
